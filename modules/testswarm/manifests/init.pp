18bf9b483466f45fca1d76aee3bc7c5e435a939e
Atul Varma <varmaa@gmail.com>
2011-04-13 21:22:33 -0700

    Added a 'swarmuser' type.

--- modules/testswarm/manifests/init.pp
+++ modules/testswarm/manifests/init.pp
@@ -52,8 +52,23 @@ class testswarm {
   }
   
   cron { "testswarm-wipe":
-    command => "curl -s --header \"Host: $site\" http://127.0.0.1/?state=wipe > /dev/null",
+    command => "/usr/bin/curl -s --header \"Host: $site\" http://127.0.0.1/?state=wipe > /dev/null",
     user => "www-data",
     require => Package['curl']
+  }
+  
+  define swarmuser( $password, $email, $request ) {
+    include testswarm
+    
+    exec { "add-testswarm-user-$name":
+      command => "/usr/bin/curl --header \"Host: $testswarm::site\" -d \"username=$name&password=$password&email=$email&request=$request\" http://127.0.0.1/signup/",
+      require => Package['curl']
+    }
+  }
+
+  swarmuser { 'hackasaurus':
+    password => 'change_me',
+    email => 'change_me@changeme.com',
+    request => 'account-for-hackasaurus-projects'
   }
 }
