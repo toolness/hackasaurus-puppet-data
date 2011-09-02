f393dcf6b7c8f82d9ff6187c4bbfbcadd962543d
Atul Varma <varmaa@gmail.com>
2011-09-02 15:33:45 -0700

    renamed setup_server.py to bootstrap.py.

--- fabfile/deployment.py
+++ fabfile/deployment.py
@@ -36,7 +36,7 @@ def deploy(server):
         'cd /root',
         'mv hackasaurus-puppet-data /var',
         'cd /var/hackasaurus-puppet-data',
-        'python fabfile/run-on-server/setup_server.py'
+        'python fabfile/run-on-server/bootstrap.py'
         ]
     result = subprocess.call(ssh_args + [';'.join(remote_cmds)])
     return result
