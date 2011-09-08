ba6d64ee0237ddb6bc0be869f4efdefdbf96c4cd
Atul Varma <varmaa@gmail.com>
2011-09-08 15:55:00 -0700

    Added a hackpub puppet module.

---
+++ modules/hackpub/manifests/init.pp
@@ -0,0 +1,9 @@
+class htmlpad {
+  $site = 'hackpub.hackasaurus.org'
+  $rootDir = '/var/hackpub.hackasaurus.org'
+  $wsgiDir = "$rootDir/wsgi"
+
+  apache2::vhost { "$site":
+    content => template("hackpub/apache-site.conf.erb")
+  }
+}
