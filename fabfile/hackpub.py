1f2d517d2b76fc168ee90d7565f03df2f69fedc1
Atul Varma <varmaa@gmail.com>
2011-09-08 17:06:32 -0700

    oops, forgot to add hackpub fabfile.

---
+++ fabfile/hackpub.py
@@ -0,0 +1,41 @@
+from StringIO import StringIO
+
+from fabric.api import *
+from fabric.contrib.files import exists
+from secrets import load_secrets
+
+PROJ_ROOT = '/var/hackpub.hackasaurus.org'
+REPO_URL = "git://github.com/hackasaurus/hackpub.git"
+
+LOCAL_SETTINGS = """\
+from settings import *
+
+AWS_ACCESS_KEY_ID = '%(hackpub_aws_access_key_id)s'
+AWS_SECRET_ACCESS_KEY = '%(hackpub_aws_secret_access_key)s'
+
+BUCKET_NAME = '%(hackpub_bucket_name)s'
+"""
+
+def run_manage_cmd(cmd):
+    with cd(PROJ_ROOT):
+        run('python manage.py %s' % cmd)
+
+@task
+def deploy():
+    if exists(PROJ_ROOT):
+        update()
+    else:
+        clone()
+    settings = StringIO(LOCAL_SETTINGS % load_secrets(env['host']))
+    put(settings, '%s/settings_local.py' % PROJ_ROOT)
+    run_manage_cmd('test')
+    run('touch %s/wsgi/hackpub.wsgi' % PROJ_ROOT)
+
+@task
+def update():
+    with cd(PROJ_ROOT):
+        run('git pull')
+
+@task
+def clone():
+    run('git clone %s %s' % (REPO_URL, PROJ_ROOT))
