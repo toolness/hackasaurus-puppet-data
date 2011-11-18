547aa183f8092abc60b5ebd93a4e25130e97505e
Atul Varma <varmaa@gmail.com>
2011-11-18 11:46:38 -0800

    changed static files dir for hackasaurus.org to /var/hackasaurus.org, since we're just rsyncing the files for deployment right now.

--- modules/hackasaurus/manifests/init.pp
+++ modules/hackasaurus/manifests/init.pp
@@ -1,7 +1,7 @@
 class hackasaurus {
   $site = 'hackasaurus.org'
   $rootDir = '/var/hackasaurus.org'
-  $staticFilesDir = "$rootDir/dist"
+  $staticFilesDir = "$rootDir"
 
   apache2::vhost { "$site":
     content => template("hackasaurus/apache-site.conf.erb")
